88790a59fda4d8f0d09304b0547743db8e12e139
Alexander Samsonov <samsonov110294@yandex.ru>
2023-08-25 19:37:02 +0500

    2я часть диплом

--- stat/endpoint-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stat/endpoint-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -5,15 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @Builder
 public class EndpointHitDto {
 
     private String app;
-
+    @NotBlank
     private String uri;
-
+    @NotBlank
     private String ip;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private String timestamp;
--- stat/stats-server/src/main/java/ru/practicum/controller/StatController.java
+++ stat/stats-server/src/main/java/ru/practicum/controller/StatController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 
 @RestController
-@Validated
 @ResponseBody
 @AllArgsConstructor
 public class StatController {
